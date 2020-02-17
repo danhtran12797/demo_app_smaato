03408a8620327621265b906afb9736d0bf38767d
Danh97 <danhtran12797@gmail.com>
2020-02-17 19:26:25 +0700

    add show Toast

--- app/src/main/java/com/danhtran12797/thd/smaatotest/MainActivity.java
+++ app/src/main/java/com/danhtran12797/thd/smaatotest/MainActivity.java
@@ -4,8 +4,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.smaato.sdk.banner.ad.BannerAdSize;
 import com.smaato.sdk.banner.widget.BannerError;
@@ -42,22 +44,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         bannerView.setEventListener(new BannerView.EventListener() {
             @Override
             public void onAdLoaded(@NonNull BannerView bannerView) {
+                Toast.makeText(MainActivity.this, "Banner load success, can show ad", Toast.LENGTH_LONG).show();
+                Log.d("AAA", "Banner load success, can show ad");
             }
 
             @Override
             public void onAdFailedToLoad(@NonNull BannerView bannerView, @NonNull BannerError bannerError) {
+                Toast.makeText(MainActivity.this, "Bannner load failed", Toast.LENGTH_LONG).show();
+                Log.d("AAA", "Bannner load failed");
             }
 
             @Override
             public void onAdImpression(@NonNull BannerView bannerView) {
+                Log.d("AAA", "Banner onAdImpression");
             }
 
             @Override
             public void onAdClicked(@NonNull BannerView bannerView) {
+                Toast.makeText(MainActivity.this, "Banner clicked", Toast.LENGTH_LONG).show();
+                Log.d("AAA", "Banner clicked");
             }
 
             @Override
             public void onAdTTLExpired(@NonNull BannerView bannerView) {
+                Log.d("AAA", "Banner onAdTTLExpired");
             }
         });
 
@@ -65,90 +75,104 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         eventListenerRewarded = new com.smaato.sdk.rewarded.EventListener() {
             @Override
             public void onAdLoaded(@NonNull RewardedInterstitialAd rewardedInterstitialAd) {
+                Toast.makeText(MainActivity.this, "RewardedAd load success, can show ad", Toast.LENGTH_LONG).show();
+                Log.d("AAA", "RewardedAd: load success");
                 rewardedInterstitialAd.showAd();
             }
 
             @Override
             public void onAdFailedToLoad(@NonNull RewardedRequestError rewardedRequestError) {
-
+                Log.d("AAA", "RewardedAd load failed");
+                Toast.makeText(MainActivity.this, "RewardedAd load failed", Toast.LENGTH_LONG).show();
             }
 
             @Override
             public void onAdError(@NonNull RewardedInterstitialAd rewardedInterstitialAd, @NonNull RewardedError rewardedError) {
-
+                Toast.makeText(MainActivity.this, "RewardedAd error", Toast.LENGTH_LONG).show();
+                Log.d("AAA", "RewardedAd error");
             }
 
             @Override
             public void onAdClosed(@NonNull RewardedInterstitialAd rewardedInterstitialAd) {
-
+                Toast.makeText(MainActivity.this, "RewardedAd closed", Toast.LENGTH_LONG).show();
+                Log.d("AAA", "RewardedAd closed");
             }
 
             @Override
             public void onAdClicked(@NonNull RewardedInterstitialAd rewardedInterstitialAd) {
-
+                Toast.makeText(MainActivity.this, "RewardedAd clicked", Toast.LENGTH_LONG).show();
+                Log.d("AAA", "RewardedAd clicked");
             }
 
             @Override
             public void onAdStarted(@NonNull RewardedInterstitialAd rewardedInterstitialAd) {
-
+                Log.d("AAA", "RewardedAd: started");
             }
 
             @Override
             public void onAdReward(@NonNull RewardedInterstitialAd rewardedInterstitialAd) {
-
+                Log.d("AAA", "RewardedAd: onAdReward");
             }
 
             @Override
             public void onAdTTLExpired(@NonNull RewardedInterstitialAd rewardedInterstitialAd) {
-
+                Log.d("AAA", "RewardedAd: onAdTTLExpired");
             }
+
         };
 
         eventListenerInterstitial = new EventListener() {
             @Override
             public void onAdLoaded(@NonNull InterstitialAd interstitialAd) {
+                Toast.makeText(MainActivity.this, "InterstitialAd load success, can show ad.", Toast.LENGTH_LONG).show();
+                Log.d("AAA", "InterstitialAd: load success");
                 interstitialAd.showAd(MainActivity.this);
             }
 
             @Override
             public void onAdFailedToLoad(@NonNull InterstitialRequestError interstitialRequestError) {
-
+                Toast.makeText(MainActivity.this, "InterstitialAd load failed", Toast.LENGTH_LONG).show();
+                Log.d("AAA", "InterstitialAd: load failed");
             }
 
             @Override
             public void onAdError(@NonNull InterstitialAd interstitialAd, @NonNull InterstitialError interstitialError) {
-
+                Toast.makeText(MainActivity.this, "InterstitialAd error", Toast.LENGTH_LONG).show();
+                Log.d("AAA", "InterstitialAd: error");
             }
 
             @Override
             public void onAdOpened(@NonNull InterstitialAd interstitialAd) {
-
+                Toast.makeText(MainActivity.this, "InterstitialAd opened", Toast.LENGTH_LONG).show();
+                Log.d("AAA", "InterstitialAd: opened");
             }
 
             @Override
             public void onAdClosed(@NonNull InterstitialAd interstitialAd) {
-
+                Toast.makeText(MainActivity.this, "InterstitialAd closed", Toast.LENGTH_LONG).show();
+                Log.d("AAA", "InterstitialAd: closed");
             }
 
             @Override
             public void onAdClicked(@NonNull InterstitialAd interstitialAd) {
+                Toast.makeText(MainActivity.this, "InterstitialAd clicked", Toast.LENGTH_LONG).show();
+                Log.d("AAA", "InterstitialAd: clicked");
 
             }
 
             @Override
             public void onAdImpression(@NonNull InterstitialAd interstitialAd) {
-
+                Log.d("AAA", "InterstitialAd: onAdImpression");
             }
 
             @Override
             public void onAdTTLExpired(@NonNull InterstitialAd interstitialAd) {
-
+                Log.d("AAA", "InterstitialAd: onAdTTLExpired");
             }
         };
 
         btnInterstitial.setOnClickListener(this);
         btnRewarded.setOnClickListener(this);
-
     }
 
     @Override
